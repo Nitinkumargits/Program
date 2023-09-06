8c87bcef1c5d1ec14354855a3c114c3193f0be51
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-06 16:00:08 +0530

    learn LogicOperator

---
+++ logicOperator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int age;
+    cout<<"Enter the age of person :";
+    cin>>age;
+    
+    if(age>=12 && age<=50){
+        cout<<"Young"<<endl;
+    }
+    else{
+        cout<<"old"<<endl;
+    }
+    if(age<=12 || age>=50){
+        cout<<"eligible for the offer";
+
+    }
+    else{
+        cout<<"Not eligible for the offer"<<endl;
+    }
+    return 0;
+
+}
